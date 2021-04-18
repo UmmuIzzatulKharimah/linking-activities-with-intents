a5d23a68649296dc7a334f89d92f495ce0c96238
UsernameAnda <prasikadroid@gmail.com>
2021-04-18 23:19:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/usingintent/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.android.usingintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.usingintent.R;
+
+public class SecondActivity extends Activity{
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
